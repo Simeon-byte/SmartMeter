be343ad76c7887e0fcd44d707438ebb7ca49b3b4
simeon-byte <saimjen.duglas@gmail.com>
2023-08-21 19:49:46 +0200

    install docker-compose

--- install.sh
+++ install.sh
@@ -5,16 +5,26 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-# Install python requirements and docker, if not present
+# Install python requirements 
 echo "Installing Python Requirements"
 pip install -r requirements.txt
+echo "Installed Python Requirements"
 
-if ! command -v docker &>/dev/null; then
+# Install docker if not present
+if ! command -v docker; then
     echo "Installing Docker..."
     curl -fsSL https://get.docker.com -o get-docker.sh
     sudo sh get-docker.sh
     rm get-docker.sh
     sudo usermod -aG docker $USER
     newgrp docker
+    sudo systemctl enable docker
     echo "Finished docker installation."
+fi
+
+# Install docker-compose if not present
+if ! command -v docker-compose; then
+    echo "Installing Docker-compose..."
+    sudo pip3 install docker-compose
+    echo "Finished docker-compose installation."
 fi
